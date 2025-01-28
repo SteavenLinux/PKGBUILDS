8750a14157715361c6d948cb899f635bbaf26beb
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-01-28 20:13:16 +0200

    vlc update

--- GitHub/Aur/vlc-luajit.sh
+++ GitHub/Aur/vlc-luajit.sh
@@ -5,6 +5,7 @@ cp vlc-luajit/PKGBUILD ~/ah
 cp vlc-luajit/add-ffmpeg7-support.patch ~/ah
 cp vlc-luajit/taglib-2.patch ~/ah
 cp vlc-luajit/update-vlc-plugin-cache.hook ~/ah
+cp vlc-luajit/libnfs6-mr-6527.patch ~/ah
 cd ~/ah
 makepkg -sr --sign
 rm -rf /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/vlc-luajit*.pkg.tar.zst
