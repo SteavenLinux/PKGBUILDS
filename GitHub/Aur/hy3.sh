91a5dd8e53a8c563a7377efdad398384672dd7e4
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-02-08 18:52:32 +0200

    ash

--- GitHub/Aur/hy3.sh
+++ GitHub/Aur/hy3.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 rm -rf ~/ah
 mkdir ~/ah
-cp asusctl-git/PKGBUILD ~/ah
+cp hy3/PKGBUILD ~/ah
 cd ~/ah
 makepkg -sr --sign
-rm -rf /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/asusctl-git*.pkg.tar.zst
-rm -rf /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/asusctl-git*.pkg.tar.zst.sig
-cp asusctl-git*.pkg.tar.zst /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/
-cp asusctl-git*.pkg.tar.zst.sig /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/
+rm -rf /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/hy3*.pkg.tar.zst
+rm -rf /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/hy3*.pkg.tar.zst.sig
+cp hy3*.pkg.tar.zst /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/
+cp hy3*.pkg.tar.zst.sig /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo/x86_64/
 
 cd /home/omarhanykasban/GitHub/SteavenLinux/SteavenRepo
 git add .
-git commit -m "Updated asusctl-git"
+git commit -m "Updated hy3"
 cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/GitHub/Aur
