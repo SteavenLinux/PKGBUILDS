1d927422a85ab9ea785526b689b96abb7c2e4486
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-03-19 22:06:03 +0200

    nvidia open is out

--- SteavenGamerYT/nvidia-utils.sh
+++ SteavenGamerYT/nvidia-utils.sh
@@ -4,19 +4,19 @@
 mkdir -p ~/ah
 
 # Remove old copy and copy fresh sources
-rm -rf ~/ah/nvidia-utils-no-open
-cp -r nvidia-utils-no-open ~/ah/nvidia-utils-no-open
-cp ~/Downloads/NVIDIA-Linux-x86_64-570.133.07.run ~/ah/nvidia-utils-no-open
+rm -rf ~/ah/nvidia-utils
+cp -r nvidia-utils ~/ah/nvidia-utils
+cp ~/Downloads/NVIDIA-Linux-x86_64-570.133.07.run ~/ah/nvidia-utils
 
 # Move to build directory
-cd ~/ah/nvidia-utils-no-open || exit 1
+cd ~/ah/nvidia-utils || exit 1
 
 # Build and sign package
 makepkg -sr --sign || exit 1
 
 # Remove old package and signature from the repo
-rm -f /mnt/nvme/SteavenRepo/x86_64/nvidia-utils-no-open*.pkg.tar.zst
-rm -f /mnt/nvme/SteavenRepo/x86_64/nvidia-utils-no-open*.pkg.tar.zst.sig
+rm -f /mnt/nvme/SteavenRepo/x86_64/nvidia-utils*.pkg.tar.zst
+rm -f /mnt/nvme/SteavenRepo/x86_64/nvidia-utils*.pkg.tar.zst.sig
 
 # Copy new package and signature to the repo
 cp *.pkg.tar.zst /mnt/nvme/SteavenRepo/x86_64
