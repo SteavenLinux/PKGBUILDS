849ef44c15132848dab42fc62baad5a583195e24
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-03-21 05:07:05 +0200

    some more pkgbuilds

---
+++ SteavenGamerYT/SteavenSettings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ensure the directory exists
+mkdir -p ~/ah
+
+# Remove old copy and copy fresh sources
+rm -rf ~/ah/SteavenSettings
+cp -r SteavenSettings ~/ah/SteavenSettings
+
+# Move to build directory
+cd ~/ah/SteavenSettings || exit 1
+
+# Build and sign package
+makepkg -sr --sign || exit 1
+
+# Remove old package and signature from the repo
+rm -f /mnt/nvme/SteavenRepo/x86_64/SteavenSettings*.pkg.tar.zst
+rm -f /mnt/nvme/SteavenRepo/x86_64/SteavenSettings*.pkg.tar.zst.sig
+
+# Copy new package and signature to the repo
+cp *.pkg.tar.zst /mnt/nvme/SteavenRepo/x86_64
+cp *.pkg.tar.zst.sig /mnt/nvme/SteavenRepo/x86_64
+
+# Change to repo directory (optional, but doesn't execute anything)
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/SteavenGamerYT
