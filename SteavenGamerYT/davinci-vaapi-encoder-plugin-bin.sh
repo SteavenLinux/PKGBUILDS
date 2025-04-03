2416a08e721d6de5d432c0ef6bc59d549a28045c
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-04-03 20:35:40 +0200

    davinci-vaapi-encoder-plugin-bin

---
+++ SteavenGamerYT/davinci-vaapi-encoder-plugin-bin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ensure the directory exists
+mkdir -p ~/ah
+
+# Remove old copy and copy fresh sources
+rm -rf ~/ah/davinci-vaapi-encoder-plugin-bin
+cp -r davinci-vaapi-encoder-plugin-bin ~/ah/davinci-vaapi-encoder-plugin-bin
+
+# Move to build directory
+cd ~/ah/davinci-vaapi-encoder-plugin-bin || exit 1
+
+# Build and sign package
+makepkg -sr --sign || exit 1
+
+# Remove old package and signature from the repo
+rm -f /mnt/nvme/SteavenRepo/x86_64/davinci-vaapi-encoder-plugin-bin*.pkg.tar.zst
+rm -f /mnt/nvme/SteavenRepo/x86_64/davinci-vaapi-encoder-plugin-bin*.pkg.tar.zst.sig
+
+# Copy new package and signature to the repo
+cp *.pkg.tar.zst /mnt/nvme/SteavenRepo/x86_64
+cp *.pkg.tar.zst.sig /mnt/nvme/SteavenRepo/x86_64
+
+# Change to repo directory (optional, but doesn't execute anything)
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/SteavenGamerYT
+
---
+++ SteavenGamerYT/davinci-vaapi-encoder-plugin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ensure the directory exists
+mkdir -p ~/ah
+
+# Remove old copy and copy fresh sources
+rm -rf ~/ah/davinci-vaapi-encoder-plugin
+cp -r davinci-vaapi-encoder-plugin ~/ah/davinci-vaapi-encoder-plugin
+
+# Move to build directory
+cd ~/ah/davinci-vaapi-encoder-plugin || exit 1
+
+# Build and sign package
+makepkg -sr --sign || exit 1
+
+# Remove old package and signature from the repo
+rm -f /mnt/nvme/SteavenRepo/x86_64/davinci-vaapi-encoder-plugin*.pkg.tar.zst
+rm -f /mnt/nvme/SteavenRepo/x86_64/davinci-vaapi-encoder-plugin*.pkg.tar.zst.sig
+
+# Copy new package and signature to the repo
+cp *.pkg.tar.zst /mnt/nvme/SteavenRepo/x86_64
+cp *.pkg.tar.zst.sig /mnt/nvme/SteavenRepo/x86_64
+
+# Change to repo directory (optional, but doesn't execute anything)
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/SteavenGamerYT
+
