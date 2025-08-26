5a12a1893fe204053c54eb2c1bb49dc9d63a2e57
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-08-26 20:41:17 +0300

    no longer needed

--- Aur/proton-cachyos-slr.sh
+++ Aur/proton-cachyos-slr.sh
@@ -4,23 +4,22 @@
 mkdir -p ~/ah
 
 # Remove old copy and copy fresh sources
-rm -rf ~/ah/zenpower3-dkms
-cp -r zenpower3-dkms ~/ah/zenpower3-dkms
+rm -rf ~/ah/proton-cachyos-slr
+cp -r proton-cachyos-slr ~/ah/proton-cachyos-slr
 
 # Move to build directory
-cd ~/ah/zenpower3-dkms || exit 1
+cd ~/ah/proton-cachyos-slr || exit 1
 
 # Build and sign package
 makepkg -sr --sign || exit 1
 
 # Remove old package and signature from the repo
-rm -f /mnt/hdd/SteavenRepo/x86_64/zenpower3-dkms*.pkg.tar.zst
-rm -f /mnt/hdd/SteavenRepo/x86_64/zenpower3-dkms*.pkg.tar.zst.sig
+rm -f /mnt/hdd/SteavenRepo/x86_64/proton-cachyos-slr*.pkg.tar.zst
+rm -f /mnt/hdd/SteavenRepo/x86_64/proton-cachyos-slr*.pkg.tar.zst.sig
 
 # Copy new package and signature to the repo
 cp *.pkg.tar.zst /mnt/hdd/SteavenRepo/x86_64
 cp *.pkg.tar.zst.sig /mnt/hdd/SteavenRepo/x86_64
 
 # Change to repo directory (optional, but doesn't execute anything)
-cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/SteavenGamerYT
-
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/Aur
