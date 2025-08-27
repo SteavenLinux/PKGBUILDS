7aafd6eb9cfb22790c50656dd212dd2b06309a84
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-08-28 01:13:42 +0300

    a

---
+++ Aur/proton-cachyos-slr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ensure the directory exists
+mkdir -p ~/ah
+
+# Remove old copy and copy fresh sources
+rm -rf ~/ah/proton-cachyos-slr
+cp -r proton-cachyos-slr ~/ah/proton-cachyos-slr
+
+# Move to build directory
+cd ~/ah/proton-cachyos-slr || exit 1
+
+# Build and sign package
+makepkg -sr --sign || exit 1
+
+# Remove old package and signature from the repo
+rm -f /mnt/hdd/SteavenRepo/x86_64/proton-cachyos-slr*.pkg.tar.zst
+rm -f /mnt/hdd/SteavenRepo/x86_64/proton-cachyos-slr*.pkg.tar.zst.sig
+
+# Copy new package and signature to the repo
+cp *.pkg.tar.zst /mnt/hdd/SteavenRepo/x86_64
+cp *.pkg.tar.zst.sig /mnt/hdd/SteavenRepo/x86_64
+
+# Change to repo directory (optional, but doesn't execute anything)
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/Aur
