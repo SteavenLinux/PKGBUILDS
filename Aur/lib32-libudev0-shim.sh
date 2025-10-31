5c81dfe7cba2d4a16ef8b2c887b058badac1d194
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-10-31 02:31:50 +0200

    updates

---
+++ Aur/lib32-libudev0-shim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ensure the directory exists
+mkdir -p ~/ah
+
+# Remove old copy and copy fresh sources
+rm -rf ~/ah/lib32-libudev0-shim
+cp -r lib32-libudev0-shim ~/ah/lib32-libudev0-shim
+
+# Move to build directory
+cd ~/ah/lib32-libudev0-shim || exit 1
+
+# Build and sign package
+makepkg -sr --sign || exit 1
+
+# Remove old package and signature from the repo
+rm -f /mnt/Omar-Old-PC/mnt/nvme/SteavenRepo/x86_64/lib32-libudev0-shim*.pkg.tar.zst
+rm -f /mnt/Omar-Old-PC/mnt/nvme/SteavenRepo/x86_64/lib32-libudev0-shim*.pkg.tar.zst.sig
+
+# Copy new package and signature to the repo
+cp *.pkg.tar.zst /mnt/Omar-Old-PC/mnt/nvme/SteavenRepo/x86_64
+cp *.pkg.tar.zst.sig /mnt/Omar-Old-PC/mnt/nvme/SteavenRepo/x86_64
+
+# Change to repo directory (optional, but doesn't execute anything)
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/Aur
