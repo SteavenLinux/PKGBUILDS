c517380ea6ecb751f59c681d0e48d669c97784f5
Omar Hany Kasban <omarhanykasban706@gmail.com>
2026-06-03 18:26:17 +0300

    updates

---
+++ Aur/flac1.3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ensure the directory exists
+mkdir -p ~/ah
+
+# Remove old copy and copy fresh sources
+rm -rf ~/ah/flac1.3
+cp -r flac1.3 ~/ah/flac1.3
+
+# Move to build directory
+cd ~/ah/flac1.3 || exit 1
+
+# Build and sign package
+makepkg -sr --sign || exit 1
+
+# Remove old package and signature from the repo
+rm -f /mnt/Omar-PC-Server/home/omarhanykasban/SteavenRepo/x86_64/flac1.3*.pkg.tar.zst
+rm -f /mnt/Omar-PC-Server/home/omarhanykasban/SteavenRepo/x86_64/flac1.3*.pkg.tar.zst.sig
+
+# Copy new package and signature to the repo
+cp *.pkg.tar.zst /mnt/Omar-PC-Server/home/omarhanykasban/SteavenRepo/x86_64
+cp *.pkg.tar.zst.sig /mnt/Omar-PC-Server/home/omarhanykasban/SteavenRepo/x86_64
+
+# Change to repo directory (optional, but doesn't execute anything)
+cd /home/omarhanykasban/GitHub/SteavenLinux/PKGBUILDS/Aur
